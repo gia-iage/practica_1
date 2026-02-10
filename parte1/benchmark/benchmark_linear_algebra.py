70bfa6ff6f4013b2fa1cc20b5664935a38a8b9d6
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-02-10 20:00:58 +0100

    Fix CPU sanitize

--- parte1/benchmark/benchmark_linear_algebra.py
+++ parte1/benchmark/benchmark_linear_algebra.py
@@ -3,6 +3,7 @@ import argparse
 import time
 import statistics
 import csv
+import re
 
 # ==================================================
 # Parámetros globales
@@ -119,6 +120,9 @@ def sanitize_cpu_name(cpu_name):
     if "@" in cpu_name:
         cpu_name = cpu_name.split("@")[0]
 
+    # Sustituir separadores problemáticos por espacio
+    cpu_name = re.sub(r"[\/\\:*?\"<>|]", " ", cpu_name)
+    
     # Limpiar espacios múltiples
     cpu_name = " ".join(cpu_name.split())
 
